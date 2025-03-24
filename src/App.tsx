82c74dd1abf019b9e41dd983da67bf4542cfd982
전준표 <junpyo0920@gmail.com>
2025-03-25 04:02:11 +0900

    chore: React Query 설치 및 설정
    - 데이터 패칭 및 캐싱 로직 최적화 및 효율화를 위해 react-query 설치
    - App.tsx에서 QueryClientProvider로 어플리케이션을 래핑하여 React Query 사용 준비
    - React Query 사용 시 권장 사항인 eslint-plugin-query 설치 및 설정 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import AppRoutes from './router';
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <AppRoutes />
-    </>
+    </QueryClientProvider>
   );
 }
 
