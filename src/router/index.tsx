0424476453ef5b8ddbc45f1e979ee79f358f4514
권상웅 <kw3082@naver.com>
2025-03-25 16:42:32 +0900

    design: 메인 레이아웃 추가

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,12 +1,15 @@
 import { Routes, Route } from 'react-router';
 import { ROUTES } from './routes';
 import Home from '../pages/home/Home.tsx';
+import MainLayout from "../layouts/MainLayOut.tsx";
 
 const AppRoutes = () => {
   return (
-    <Routes>
-      <Route path={ROUTES.HOME} element={<Home />} />
-    </Routes>
+      <MainLayout>
+          <Routes>
+              <Route path={ROUTES.HOME} element={<Home />} />
+          </Routes>
+      </MainLayout>
   );
 };
 
