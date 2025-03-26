e9a70c3a17215911c2603b3ba4e8574497e80fcf
권상웅 <kw3082@naver.com>
2025-03-26 17:13:21 +0900

    design: MainLayout 컴포넌트에서 내부 패딩 제거

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -7,7 +7,7 @@ interface MainLayoutProps {
 const MainLayout = ({ children }: MainLayoutProps) => {
     return (
         <div className="min-h-screen bg-black">
-            <div className="max-w-[1680px] mx-auto px-4 sm:px-6 lg:px-8">
+            <div className="max-w-[1680px] mx-auto">
                 {children}
             </div>
         </div>
