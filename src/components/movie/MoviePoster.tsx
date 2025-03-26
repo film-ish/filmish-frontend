22a46e20a92126d46e6b8c2e0bd8cb811e2af64a
전준표 <junpyo0920@gmail.com>
2025-03-26 17:19:46 +0900

    feat: 재사용 가능한 MoviePoster 컴포넌트 추가

---
+++ src/components/movie/MoviePoster.tsx
@@ -0,0 +1,25 @@
+import LikeButton from './LikeButton';
+
+interface MoviePosterProps {
+  posterSrc: string;
+  width?: number;
+  liked?: boolean;
+  onLike?: () => void;
+}
+
+const MoviePoster = ({ posterSrc, width, liked, onLike }: MoviePosterProps) => {
+  return (
+    <div
+      className="relative w-full rounded-[10px] overflow-hidden aspect-[1/1.42] flex items-center justify-center"
+      style={{ width: width }}>
+      <img
+        className="object-cover w-full"
+        src={posterSrc ? posterSrc : 'https://picsum.photos/200/300'}
+        alt="poster image"
+      />
+      {liked !== undefined && onLike && <LikeButton liked={liked} onClick={onLike} />}
+    </div>
+  );
+};
+
+export default MoviePoster;
