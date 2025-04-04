cbe8535136408d46e89afd16868159b5ae2a0e9b
권상웅 <kw3082@naver.com>
2025-04-04 11:05:18 +0900

    feat: response.data -> response - header에서 accesstoken을 받기 위해서

--- src/api/login/loginApi.ts
+++ src/api/login/loginApi.ts
@@ -6,9 +6,9 @@ import { apiClient } from '../instance/client';
 const login = async (formData: { email: string; password: string }) => {
   // axios.post를 사용하여 '/api/login' 엔드포인트로 POST 요청을 보냅니다.
   // 요청 본문(body)에는 formData를 전달합니다.
-  const response = await apiClient.post('/login', formData);
+  const response = await apiClient.post('/users/login', formData);
   // API 응답 객체에서 data 속성 (실제 서버 데이터)을 반환합니다.
-  return response.data;
+  return response;
 };
 
 // login 함수를 외부에서 사용할 수 있도록 export 합니다.
