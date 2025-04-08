7d0f7999037a5e86d723eb9d96813e315e27fc85
권상웅 <kw3082@naver.com>
2025-04-08 13:54:00 +0900

    fix: router conflict 해결

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -9,8 +9,6 @@ import Recommendations from '../pages/genre/Recommendations.tsx';
 import MovieTalkDetail from '../pages/movie-talk/movie-talk-detail/MovieTalkDetail.tsx';
 import Login from '../pages/login/Login.tsx';
 import SignUp from '../pages/sign-up/SignUp.tsx';
-<<<<<<< Updated upstream
-import CommercialMain from '../pages/commercial';
 import MovieDetailLayout from '../layouts/MovieDetailLayout.tsx';
 import DetailPage from '../pages/movie-detail/DetailPage.tsx';
 import MovieRatingsPage from '../pages/movie-detail/RatingsPage.tsx';
@@ -20,13 +18,8 @@ import ReviewDetailPage from '../pages/movie-detail/review/ReivewDetailPage.tsx'
 import HomeRecommendations from '../pages/home/more/HomeRecommendations.tsx';
 import HomeTopRated from '../pages/home/more/HomeTopRated.tsx';
 import HomeTopLiked from '../pages/home/more/HomeTopLiked.tsx';
-=======
 import CommercialMain from "../pages/commercial";
-import HomeRecommendations from "../pages/home/more/HomeRecommendations.tsx";
-import HomeTopRated from "../pages/home/more/HomeTopRated.tsx";
-import HomeTopLiked from "../pages/home/more/HomeTopLiked.tsx";
 import Search from '../pages/search/Search';
->>>>>>> Stashed changes
 
 const AppRoutes = () => {
   return (
@@ -36,7 +29,6 @@ const AppRoutes = () => {
         <Route path={ROUTES.MOVIE_TALK} element={<MovieTalk />} />
         <Route path={ROUTES.MOVIE_TALK_DETAIL} element={<MovieTalkDetail />} />
 
-<<<<<<< Updated upstream
         <Route path={ROUTES.GENRE.ROOT}>
           <Route index element={<List />} />
           <Route path={ROUTES.GENRE.DETAIL} element={<Detail />} />
@@ -61,26 +53,6 @@ const AppRoutes = () => {
       </Routes>
     </MainLayout>
   );
-=======
-                <Route path={ROUTES.GENRE.ROOT}>
-                    <Route index element={<List />} />
-                    <Route path={ROUTES.GENRE.DETAIL} element={<Detail />} />
-                    <Route path={`${ROUTES.GENRE.DETAIL}/${ROUTES.GENRE.RECOMMENDATIONS}`} element={<Recommendations />} />
-                </Route>
-                <Route path={ROUTES.LOGIN} element={<Login />} />
-                <Route path={ROUTES.SIGN_UP} element={<SignUp />} />
-                <Route path={ROUTES.COMMERCIAL} element={<CommercialMain/>}/>
-                
-                {/* 홈 더보기 페이지 라우트 */}
-                <Route path={ROUTES.HOME_MORE.RECOMMENDATIONS} element={<HomeRecommendations />} />
-                <Route path={ROUTES.HOME_MORE.TOP_RATED} element={<HomeTopRated />} />
-                <Route path={ROUTES.HOME_MORE.TOP_LIKED} element={<HomeTopLiked />} />
-
-                <Route path={ROUTES.SEARCH} element={<Search />} />
-            </Routes>
-        </MainLayout>
-    );
->>>>>>> Stashed changes
 };
 
 export default AppRoutes;
