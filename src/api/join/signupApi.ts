97b9e9ae6fca0c341f0808dc6b492023a9b83a05
최지원 <chjw956@naver.com>
2025-04-08 21:28:22 +0900

    feat: 회원가입 페이지 구현
    - 회원가입 API 연동
    - 닉네임 중복 확인 API 연동
    - 이메일 중복 확인 API 연동
    - 이미지 크기 및 확장자 확인 기능 구현

--- src/api/join/signupApi.ts
+++ src/api/join/signupApi.ts
@@ -1,8 +1,27 @@
 import { apiClient } from "../instance/client"
 
-const signup = async (formData: {   email: string, password: string, nickname: string,birth: string, image: string }) => {
-  const response = await apiClient.post('/users', formData);
+// const signup = async (formData: {   email: string, password: string, nickname: string,birth: string, image: string }) => {
+const signup = async (formData: FormData) => {
+  const response = await apiClient.post('/users', formData, {
+    headers: {
+      "Content-Type": "multipart/form-data", // 직접 설정도 가능
+    },
+  });
   return response;
 };
 
-export { signup };
+const checkEmail = async (email: string) => {
+  const response = await apiClient.get(`/users/email`,{
+    params: {value: email}
+  });
+  return response;
+};
+
+const checkNickname = async (nickname: string) => {
+  const response = await apiClient.get(`/users/nickname`, {
+    params: {value: nickname}
+  });
+  return response;
+}
+
+export { signup, checkEmail, checkNickname };
